c83b63e36afbd59b3721d55223f001ed70c3b172
Sohini Purkait <purkaitsohini20@gmail.com>
2024-07-18 10:27:56 +0530

    line group added

--- script.js
+++ script.js
@@ -2,3 +2,45 @@ const navDialog = document.getElementById('nav-dialog');
 function handleMenu(){
     navDialog.classList.toggle('hidden');
 }
+
+const initialTranslateLTR = -48*4;
+const initialTranslateRTL = 36*4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting) {
+            document.addEventListener('scroll', scrollHandler);
+        } else {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+
+    intersectionObserver.observe(element);
+
+    function scrollHandler() {
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR) {
+            totalTranslate = translateX + initialTranslateLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranslateRTL);
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+const line4 = document.getElementById('line4');
+
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
+setupIntersectionObserver(line4, true, 0.8);
